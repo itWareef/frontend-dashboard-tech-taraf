f211dbd2fa22fe41a0ba7151b716b6f9e488608e
abdelrhmanali <abdelrhmanali1812@gamil.com>
2025-07-30 03:09:24 +0300

    merge and updete ui setting menu

--- src/components/shared/svgIcons.jsx
+++ src/components/shared/svgIcons.jsx
@@ -18,13 +18,23 @@ import returnIcon from "../../assets/Icons/returnIcon.svg";
 import userGroups from "../../assets/Icons/usersGroups.svg";
 import userGroups2 from "../../assets/Icons/usersGroups2.svg";
 import warning from "../../assets/Icons/warning.svg";
+import udateUser from "../../assets/Icons/update-user.svg";
+import editIcon from "../../assets/Icons/edit-icon.svg";
+import arrowLeftS from "../../assets/Icons/arrow-left.svg";
+import builds from "../../assets/Icons/build-icon.svg";
+import maintance from "../../assets/Icons/maintenance-icon.svg";
+import plant from "../../assets/Icons/plant-icon.svg";
+import notificationsIconS from "../../assets/Icons/notification-icon.svg";
+import time from "../../assets/Icons/time-icon.svg";
+import mode from "../../assets/Icons/mode-icon.svg";
+import language from "../../assets/Icons/languag-icon.svg";
 
 export const svgIcons = {
   proposalSubmission: proposalSubmission,
   ordersIcon: ordersIcon,
   returnIcon: returnIcon,
   listIcon: listIcon,
-  notificationsIcon: notificationsIcon,
+  S: notificationsIcon,
   logoutIcon: logoutIcon,
   fullScreenIcon: fullScreenIcon,
   chatIcon: chatIcon,
@@ -40,7 +50,16 @@ export const svgIcons = {
   checkCircle: checkCircle,
   filterIcon: filterIcon,
   add: add,
-
+  udateUser: udateUser,
+  editIcon: editIcon,
+  arrowLeftS: arrowLeftS,
+  builds: builds,
+  maintance: maintance,
+  plant: plant,
+  notificationsIconS: notificationsIconS,
+  time: time,
+  mode: mode,
+  language: language,
   projects: (
     <svg
       width="32"
